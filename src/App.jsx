8434894dd1e0145070096cfeb10f3d253c51e985
frrann <ada.fran92@gmail.com>
2023-08-25 14:32:24 +0300

    Implement mobile-first styles for basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import { IoMdSearch } from "react-icons/io";
+import { RiMoonFill } from "react-icons/ri";
+import { RiMoonLine } from "react-icons/ri";
+import "./styles/globals.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("dark");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={`theme-${theme}`}>
+      <div className="container">
+        <header>
+          <div className="logo">
+            <p>Where in the world?</p>
+          </div>
+          <div
+            className="theme-switch"
+            onClick={() =>
+              setTheme((crtState) => (crtState === "dark" ? "light" : "dark"))
+            }
+          >
+            {theme === "dark" ? <RiMoonFill /> : <RiMoonLine />}
+            <p>Dark Mode</p>
+          </div>
+        </header>
+        <main className="main">
+          <div className="filters">
+            <div className="search">
+              <IoMdSearch />
+              <input
+                id="search"
+                type="text"
+                placeholder="Search for a country..."
+              />
+            </div>
+            <div className="filter">
+              <select id="filter">
+                <option value="none">Filter by Region</option>
+                <option value="Africa">Africa</option>
+                <option value="America">America</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
+              </select>
+            </div>
+          </div>
+          <div className="countries">
+            <div className="country card">
+              <img className="country__flag" src="" alt="$Country flag" />
+              <p className="country__name">$Name</p>
+              <ul className="country__details">
+                <li className="country__population">$Population</li>
+                <li className="country__region">$Region</li>
+                <li className="country__capital">$Capital</li>
+              </ul>
+            </div>
+            <div className="country card">
+              <img className="country__flag" src="" alt="$Country flag" />
+              <p className="country__name">$Name</p>
+              <ul className="country__details">
+                <li className="country__population">$Population</li>
+                <li className="country__region">$Region</li>
+                <li className="country__capital">$Capital</li>
+              </ul>
+            </div>
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
