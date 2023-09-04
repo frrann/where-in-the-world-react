f471f1c67cf8cddb3f598fd0116b12204f0ba61f
frrann <ada.fran92@gmail.com>
2023-09-04 15:16:26 +0300

    Implement country details page with mock data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
-const Card = ({ children, className }) => {
-  return <div className={`${className} card`}>{children}</div>;
+const Card = ({ children, className, onClick }) => {
+  return (
+    <div className={`${className ? className : null} card`} onClick={onClick}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,14 +1,11 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
 import CountryItem from "./CountryItem";
 
 const CountryList = ({ countries }) => {
   return (
     <div className="countries">
       {countries.map((country) => (
-        <Link to={country.name} key={country.alpha3Code}>
-          <CountryItem country={country} />
-        </Link>
+        <CountryItem country={country} key={country.alpha3Code} />
       ))}
     </div>
   );
